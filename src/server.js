9c4ecfcfce787dbef2d26bfadf0a38459e201b6e
sourav11 <143427277+Sourav0211@users.noreply.github.com>
2023-12-09 23:05:47 -0500

    2

--- src/server.js
+++ src/server.js
@@ -190,5 +190,5 @@ app.post('/api/register', async (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Server is running at http:localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
